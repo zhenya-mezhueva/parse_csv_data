54d1b4a5306e3ea00e569ecb99ef393f8ee905a0
Zhenya <e.mezhueva@gmail.com>
2021-02-08 14:19:12 +0200

    basic styles and parse

--- src/App.js
+++ src/App.js
@@ -1,8 +1,66 @@
+import React, { useState } from 'react';
+import { parse } from 'papaparse';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [info, setInfo] = useState([]);
+
+  const readText = async (data) => {
+    const text = await data.text();
+    const result = parse(text, { header: true });
+    console.log(result);
+    return result.data;
+  }
+
   return (
-    <div className="App">
+    <div className="container">
+      <h1 className="title">Parse CSV</h1>
+      <div className="inp-area">
+        <input type="file"
+         className="custom-input"
+         accept=".csv"
+         onChange={event => {
+           const file = event.target.files[0];
+           setInfo(readText(file));
+         }}
+        />
+      </div>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Full Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Age</th>
+            <th>Experience</th>
+            <th>Yearly Income</th>
+            <th>Has children</th>
+            <th>License states</th>
+            <th>Expiration date</th>
+            <th>License number</th>
+            <th>Duplicate</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+        </tbody>
+
+      </table>
 
     </div>
   );
